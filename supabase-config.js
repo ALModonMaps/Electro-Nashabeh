b392057e396cecc437730bed9ad15664b2f3ddca
ALModonMap <ali.crepto.90@gmail.com>
2026-08-20 14:46:37 +0300

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,3 @@
+const SUPABASE_URL="https://cllkjoweasclsrhtyhmg.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY="sb_publishable_i8tdfU7b3UJ74W_MzoKIWQ_JymASamC";
+const sb=supabase.createClient(SUPABASE_URL,SUPABASE_PUBLISHABLE_KEY);
